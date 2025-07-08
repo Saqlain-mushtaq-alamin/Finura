47ee62a1908a500be73a4b61ab0f632e8a6e2bb7
Saqlain-mushtaq-alamin <alamin86911@gmail.com>
2025-07-08 20:02:08 +0600

    home page input field edit done

--- finura_frontend/lib/views/homePage.dart
+++ finura_frontend/lib/views/homePage.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
   final String userFirstName;
   final String userProfilePicUrl;
 
-  const HomePage({
+  final TextEditingController categoryController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
+  String? selectedOption;
+
+  HomePage({
     Key? key,
     required this.userFirstName,
     required this.userProfilePicUrl,
@@ -16,16 +21,18 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         elevation: 1,
-        backgroundColor: Colors.white,
+        backgroundColor: const Color.fromARGB(255, 164, 245, 171),
         titleSpacing: 0,
         title: Row(
           children: [
             // User profile picture
             Padding(
-              padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+              padding: const EdgeInsets.only(left: 20.0, right: 16.0),
               child: CircleAvatar(
                 radius: 20,
-                backgroundImage: NetworkImage(userProfilePicUrl),
+                backgroundImage: NetworkImage(
+                  userProfilePicUrl,
+                ), // Replace with a local asset if needed
                 backgroundColor: Colors.grey[200],
               ),
             ),
@@ -49,10 +56,163 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: Container(), // No body content for now 
+
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16.0),
+        color: const Color.fromARGB(255, 235, 250, 235),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 16),
+
+            // Box 1: Input Field
+            Container(
+              padding: const EdgeInsets.all(12),
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Column(
+                children: [
+                  // Text fields for category and amount
+                  TextField(
+                    controller: categoryController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                      hintText:
+                          'Enter category here', // This should be changed when i found something better'
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextField(
+                    controller: amountController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                      hintText: 'Enter amount here',
+                    ),
+                    keyboardType: const TextInputType.numberWithOptions(
+                      decimal: true,
+                      signed: false,
+                    ),
+                    inputFormatters: [
+                      // Allow only numbers and decimal point
+                      FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')),
+                    ],
+                  ),
+
+                  const SizedBox(height: 8),
+                  DropdownButtonFormField<String>(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                    ),
+                    hint: const Text('Select an option'),
+
+                    items: const [
+                      DropdownMenuItem(
+                        value: 'ExpenseOption',
+                        child: Text('Expense'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'IncomeOption',
+                        child: Text('Income'),
+                      ),
+                    ],
+                    onChanged: (value) {
+                      selectedOption = value;
+                      // Update the state or handle selection
+                      // Handle selection
+                    },
+                  ),
+
+                  //submit button
+                  const SizedBox(height: 8),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50.0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Handle submit action
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                      child: const Text('Submit'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Box 2: Dropdown Menu
+            Container(
+              padding: const EdgeInsets.all(12),
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: DropdownButtonFormField<String>(
+                decoration: const InputDecoration.collapsed(hintText: ''),
+                hint: const Text('Select an option'),
+                items: const [
+                  DropdownMenuItem(value: 'option1', child: Text('Option 1')),
+                  DropdownMenuItem(value: 'option2', child: Text('Option 2')),
+                  DropdownMenuItem(value: 'option3', child: Text('Option 3')),
+                ],
+                onChanged: (value) {
+                  // Handle selection
+                },
+              ),
+            ),
+
+            // Box 3: Button
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle button press
+                },
+                child: const Text('Show Notification'),
+              ),
+            ),
+
+            // Box 4: Notification Display
+            Container(
+              padding: const EdgeInsets.all(12),
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.yellow[100],
+                border: Border.all(color: Colors.orange),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Text(
+                'Notification will appear here.',
+                style: TextStyle(color: Colors.black87),
+              ),
+            ),
+          ],
+        ),
+      ),
+
       bottomNavigationBar: BottomAppBar(
         elevation: 8,
-        color: Colors.white,
+        color: const Color.fromARGB(255, 164, 245, 171),
         child: SizedBox(
           height: 60,
           child: Row(
