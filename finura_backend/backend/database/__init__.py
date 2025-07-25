211d57272da6cff7fde1ff15ae1a2d9acfe51f34
Saqlain-mushtaq-alamin <alamin86911@gmail.com>
2025-07-25 21:48:48 +0600

    fixed the error  server backend database connoction rewrite all code again

--- finura_backend/backend/database/__init__.py
+++
@@ -1,8 +0,0 @@
-# backend/database/__init__.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/finura"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
