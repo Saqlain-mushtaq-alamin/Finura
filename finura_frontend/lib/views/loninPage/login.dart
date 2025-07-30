b8241649ed3932dad75de83b74376aaeb838f03d
Saqlain-mushtaq-alamin <alamin86911@gmail.com>
2025-07-30 23:32:37 +0600

    make login page  non return able

--- finura_frontend/lib/views/loninPage/login.dart
+++ finura_frontend/lib/views/loninPage/login.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
-
 import 'package:finura_frontend/services/local_database/local_database_helper.dart';
 import 'package:finura_frontend/views/HomePage.dart';
 import 'package:finura_frontend/views/loninPage/registerPage.dart';
 import 'package:flutter/material.dart';
 import 'package:crypto/crypto.dart';
+import 'package:flutter/services.dart'; // To use SystemNavigator
 
 class LoginPage extends StatelessWidget {
   final TextEditingController accountController = TextEditingController();
@@ -12,6 +12,7 @@ class LoginPage extends StatelessWidget {
 
   LoginPage({super.key});
 
+  // This method is responsible for checking user credentials
   Future<int> _getIdPin(String accountName, String pin) async {
     final db = await FinuraLocalDbHelper().database;
 
@@ -64,138 +65,146 @@ class LoginPage extends StatelessWidget {
     }
   }
 
+  // This method prevents the back action and closes the app
+  Future<bool> _onWillPop() async {
+    // Close the app when back button is pressed
+    SystemNavigator.pop();
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          color: Colors.green[100], // Fixed invalid shade
-          alignment: Alignment.center,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 48),
-                const Text(
-                  'Finura',
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    letterSpacing: 2,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                const Text(
-                  'increase your financial aura',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                    fontStyle: FontStyle.italic,
-                  ),
-                  //textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 16),
-
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(32),
-                    padding: const EdgeInsets.all(32.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: const BorderRadiusDirectional.only(
-                        topStart: Radius.circular(8.0),
-                        topEnd: Radius.circular(50.0),
-                        bottomStart: Radius.circular(50.0),
-                        bottomEnd: Radius.circular(8.0),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
+    return WillPopScope(
+      onWillPop: _onWillPop, // Intercept the back button press
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            color: Colors.green[100], // Color of the screen
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 48),
+                  const Text(
+                    'Finura',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      letterSpacing: 2,
                     ),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 70),
+                  ),
+                  const SizedBox(height: 8),
+                  const Text(
+                    'increase your financial aura',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
 
-                          TextField(
-                            controller: accountController,
-                            decoration: const InputDecoration(
-                              labelText: 'Account name',
-                              border: OutlineInputBorder(),
-                            ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(32),
+                      padding: const EdgeInsets.all(32.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: const BorderRadiusDirectional.only(
+                          topStart: Radius.circular(8.0),
+                          topEnd: Radius.circular(50.0),
+                          bottomStart: Radius.circular(50.0),
+                          bottomEnd: Radius.circular(8.0),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: const Offset(0, 3),
                           ),
-                          const SizedBox(height: 16),
-                          TextField(
-                            controller: pinController,
-                            obscureText: true,
-                            keyboardType: TextInputType.number,
-                            decoration: const InputDecoration(
-                              labelText: 'Enter PIN',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                          const SizedBox(height: 16),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              TextButton(
-                                onPressed: () {},
-                                child: const Text('Forget PIN?'),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => RegisterPage(),
-                                    ),
-                                  );
-                                },
-                                child: const Text('Register Now'),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 32),
-                          SizedBox(
-                            width: double.infinity,
-                            child: ElevatedButton(
-                              onPressed: () {
-                                _getIdPin(
-                                  accountController.text.trim(),
-                                  pinController.text.trim(),
-                                ).then((result) {
-                                  if (result == 1) {
-                                    // Successfully logged in
-                                    ScaffoldMessenger.of(
-                                      FinuraLocalDbHelper
-                                          .navigatorKey
-                                          .currentContext!,
-                                    ).showSnackBar(
-                                      const SnackBar(
-                                        content: Text('Login successful'),
-                                      ),
-                                    );
-                                  }
-                                });
-                              },
-                              child: const Text('Login'),
-                            ),
-                          ),
-                          const SizedBox(height: 16),
                         ],
                       ),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 70),
+                            TextField(
+                              controller: accountController,
+                              decoration: const InputDecoration(
+                                labelText: 'Account name',
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            TextField(
+                              controller: pinController,
+                              obscureText: true,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                labelText: 'Enter PIN',
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Forget PIN?'),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => RegisterPage(),
+                                      ),
+                                    );
+                                  },
+                                  child: const Text('Register Now'),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 32),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  _getIdPin(
+                                    accountController.text.trim(),
+                                    pinController.text.trim(),
+                                  ).then((result) {
+                                    if (result == 1) {
+                                      // Successfully logged in
+                                      ScaffoldMessenger.of(
+                                        FinuraLocalDbHelper
+                                            .navigatorKey
+                                            .currentContext!,
+                                      ).showSnackBar(
+                                        const SnackBar(
+                                          content: Text('Login successful'),
+                                        ),
+                                      );
+                                    }
+                                  });
+                                },
+                                child: const Text('Login'),
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
