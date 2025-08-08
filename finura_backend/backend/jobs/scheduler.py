270c3e44e175a5a91adaaee11181e0b3a8094c46
Saqlain-mushtaq-alamin <alamin86911@gmail.com>
2025-08-08 18:46:59 +0600

    write all code for the scheduler.py file

---
+++ finura_backend/backend/jobs/scheduler.py
@@ -0,0 +1,23 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from backend.database.database import SessionLocal
+from backend.database.models import User
+from backend.services.expense_service import get_last_7_days_expenses
+from backend.ml_predictor.predictor import predict_next_day
+from backend.ml_predictor.preprocess import preprocess
+
+def run_prediction_job():
+    db = SessionLocal()
+    users = db.query(User).all()
+
+    for user in users:
+        data = get_last_7_days_expenses(db, user.id)
+        if len(data) == 7:
+            input_data = preprocess(data)
+            prediction = predict_next_day(input_data)
+            print(f"Prediction for {user.email}: {prediction}")
+    db.close()
+
+def start_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(run_prediction_job, 'cron', hour=23, minute=59)
+    scheduler.start()
