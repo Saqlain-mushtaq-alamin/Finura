d7790e9fa5c42e16e2d0d439a315523cda1caa6b
Saqlain-mushtaq-alamin <alamin86911@gmail.com>
2025-09-08 13:30:47 +0600

    add intial code for fcm services

---
+++ finura_backend/backend/services/fcm_service.py
@@ -0,0 +1,21 @@
+# backend/services/fcm_service.py
+
+import firebase_admin
+from firebase_admin import credentials, messaging
+
+cred = credentials.Certificate("firebase.json")
+firebase_admin.initialize_app(cred)
+
+def send_fcm_notification(token: str, title: str, body: str, data: dict = None):
+    if not token:
+        return None
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=body
+        ),
+        token=token,
+        data={k: str(v) for k, v in (data or {}).items()}
+    )
+    response = messaging.send(message)
+    return response
